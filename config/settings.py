f5ce619b2a0f0387ce6bac816270942efe717e91
SenpaiDark <ateredaniel545@gmail.com>
2026-06-30 22:16:10 +0100

    Fix ALLOWED_HOSTS for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',') + ['mayor4code.onrender.com']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
